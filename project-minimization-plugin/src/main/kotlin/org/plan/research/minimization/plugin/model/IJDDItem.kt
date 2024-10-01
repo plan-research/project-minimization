0d9855ffd853d9528f6edb01eeea32f2af23eaa2
Sergei Kharitontcev-Beglov <khbminus@gmail.com>
2024-10-01 10:40:44 +0200

    Removed unnecessary functions

--- project-minimization-plugin/src/main/kotlin/org/plan/research/minimization/plugin/model/IJDDItem.kt
+++ project-minimization-plugin/src/main/kotlin/org/plan/research/minimization/plugin/model/IJDDItem.kt
@@ -4,8 +4,5 @@ import com.intellij.openapi.vfs.VirtualFile
 import org.plan.research.minimization.core.model.DDItem
 
 sealed interface IJDDItem : DDItem {
-    fun getChildren(): List<IJDDItem>
-    data class VirtualFileDDItem(val vfs: VirtualFile): IJDDItem {
-        override fun getChildren(): List<VirtualFileDDItem> = vfs.children.map { VirtualFileDDItem(it) }
-    }
+    data class VirtualFileDDItem(val vfs: VirtualFile): IJDDItem
 }

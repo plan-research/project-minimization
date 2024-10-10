185a8e291b27022646ef900b8fa196fe3f7a89b3
Sergei Kharitontcev-Beglov <Sergei.Kharitontcev-Beglov@jetbrains.com>
2024-10-10 12:00:42 +0200

    Added KDoc

--- project-minimization-plugin/src/main/kotlin/org/plan/research/minimization/plugin/execution/gradle/GradleBuildExceptionProvider.kt
+++ project-minimization-plugin/src/main/kotlin/org/plan/research/minimization/plugin/execution/gradle/GradleBuildExceptionProvider.kt
@@ -23,10 +23,28 @@ import org.plan.research.minimization.plugin.errors.CompilationPropertyCheckerEr
 import org.plan.research.minimization.plugin.execution.IdeaCompilationException
 import org.plan.research.minimization.plugin.model.BuildExceptionProvider
 
+/**
+ * An implementation of the [BuildExceptionProvider]
+ * that checks the compilation status of a given Gradle-based project.
+ *
+ * The implementation for now is based on the `clean` and `compileKotlin` tasks.
+ * However, TODO is to add a setting to make work with other tasks
+ */
 class GradleBuildExceptionProvider(private val cs: CoroutineScope) : BuildExceptionProvider {
     private val gradleOutputParser = KotlincOutputParser()
+
+    /**
+     * Checks the compilation of the given project, ensuring it has the necessary Gradle tasks and runs them.
+     *
+     * @param project The project to check compilation for.
+     * @return `List<BuildEvent>` if the compilation has been failed by kotlinc.
+     * Each [BuildEvent][com.intellij.build.events.BuildEvent] contains some error
+     *
+     * or [CompilationPropertyCheckerError] if the compilation has been successful or some other error occurred
+     */
     override suspend fun checkCompilation(project: Project) = either {
         val externalProjectPath = project.guessProjectDir()?.path
+        // Fails then and only then, when this project is default
         ensureNotNull(externalProjectPath) { CompilationPropertyCheckerError.InvalidBuildSystem }
         val gradleExecutionHelper = GradleExecutionHelper()
         val gradleTasks = catch(
@@ -39,6 +57,7 @@ class GradleBuildExceptionProvider(private val cs: CoroutineScope) : BuildExcept
             },
             { it: Throwable -> raise(CompilationPropertyCheckerError.BuildSystemFail(cause = it)) }
         )
+        // If not gradle tasks were found ⇒ this is not a Gradle project
         ensure(gradleTasks.isNotEmpty()) { CompilationPropertyCheckerError.InvalidBuildSystem }
         val buildTask = gradleTasks.firstOrNull { it.name == "build" }
         val cleanTask = gradleTasks.firstOrNull { it.name == "clean" }
@@ -52,6 +71,17 @@ class GradleBuildExceptionProvider(private val cs: CoroutineScope) : BuildExcept
         parseResults("test-build-${project.name}", buildResult)
     }
 
+    /**
+     * Executes a Gradle task for the given project and returns the result or an error if the task fails.
+     *
+     * Basically just contains a lot of boilerplate code to make IDEA work.
+     * FIXME: Extract it to another module
+     *
+     * @param project Project instance where the Gradle task will be run.
+     * @param task The specific GradleTask to be executed within the project.
+     * @return Either a [CompilationPropertyCheckerError] if an error occurs during task execution,
+     *         or a [GradleConsoleRunResult] containing the results of the task execution.
+     */
     private suspend fun runTask(
         project: Project,
         task: GradleTask,
@@ -76,6 +106,7 @@ class GradleBuildExceptionProvider(private val cs: CoroutineScope) : BuildExcept
             .build()
 
         val runner = ProgramRunner.getRunner(executor.id, configuration)
+        // Don't know any situation when it could happen
         ensureNotNull(runner) { CompilationPropertyCheckerError.InvalidBuildSystem }
 
         executionEnvironment.setCallback { descriptor ->
@@ -84,20 +115,27 @@ class GradleBuildExceptionProvider(private val cs: CoroutineScope) : BuildExcept
                 ?.addProcessListener(processAdapter)
         }
         writeAction {
-            catch(
+            catch( // runner.execute can throw
                 { runner.execute(executionEnvironment) },
                 { raise(CompilationPropertyCheckerError.BuildSystemFail(it)) }
             )
         }
-        processAdapter.getRunResult()
+        processAdapter.getRunResult() // Wait until writeAction is done
     }
 
+    /**
+     * Parses the results of a Gradle console run and constructs an [IdeaCompilationException] using the parsed errors.
+     *
+     * @param eventId The identifier for the event in the build process.
+     * @param results The [GradleConsoleRunResult] containing the standard error output from the Gradle run.
+     * @return An [IdeaCompilationException] containing a list of build errors.
+     */
     private fun parseResults(eventId: String, results: GradleConsoleRunResult): IdeaCompilationException {
         val outputReader = StringBuildOutputInstantReader.create(eventId, results.stdErr)
         val parsedErrors = buildList {
             while (true) {
                 val line = outputReader.readLine() ?: break
-                if(!gradleOutputParser.parse(line, outputReader, this::add))
+                if (!gradleOutputParser.parse(line, outputReader, this::add))
                     break
             }
         }

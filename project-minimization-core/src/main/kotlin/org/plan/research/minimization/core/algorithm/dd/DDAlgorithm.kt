09f8f2a37fd16a9de7b2acaf52171b199619dffd
Rustam Sadykov <sbonekenobi@gmail.com>
2024-10-10 11:23:40 +0200

    Add some comments

--- project-minimization-core/src/main/kotlin/org/plan/research/minimization/core/algorithm/dd/DDAlgorithm.kt
+++ project-minimization-core/src/main/kotlin/org/plan/research/minimization/core/algorithm/dd/DDAlgorithm.kt
@@ -4,6 +4,12 @@ import org.plan.research.minimization.core.model.DDItem
 import org.plan.research.minimization.core.model.DDContext
 import org.plan.research.minimization.core.model.PropertyTester
 
+/**
+ * Interface representing a Delta Debugging algorithm used to minimize a set of items.
+ *
+ * The `minimize` function works with a given context and a list of items, using a property tester to
+ * identify and minimize the subset of items that cause a specific error or property.
+ */
 interface DDAlgorithm {
     suspend fun <C : DDContext, T : DDItem> minimize(
         context: C, items: List<T>,
@@ -11,6 +17,14 @@ interface DDAlgorithm {
     ): DDAlgorithmResult<C, T>
 }
 
+/**
+ * DDAlgorithmResult is a data class representing the result of an algorithm execution.
+ *
+ * @param C the type of context associated with the algorithm.
+ * @param T the type of items produced by the algorithm.
+ * @property context the context in which the algorithm was executed.
+ * @property items the list of items produced by the algorithm.
+ */
 data class DDAlgorithmResult<C : DDContext, T : DDItem>(
     val context: C,
     val items: List<T>,

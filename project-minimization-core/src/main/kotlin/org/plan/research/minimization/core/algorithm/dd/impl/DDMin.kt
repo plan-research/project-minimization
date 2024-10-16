3221f86976ff6505b47cbac282bddee34fe67a45
Rustam Sadykov <sbonekenobi@gmail.com>
2024-10-16 12:31:07 +0200

    fix diktat for core

--- project-minimization-core/src/main/kotlin/org/plan/research/minimization/core/algorithm/dd/impl/DDMin.kt
+++ project-minimization-core/src/main/kotlin/org/plan/research/minimization/core/algorithm/dd/impl/DDMin.kt
@@ -1,12 +1,13 @@
 package org.plan.research.minimization.core.algorithm.dd.impl
 
-import kotlinx.coroutines.yield
 import org.plan.research.minimization.core.algorithm.dd.DDAlgorithm
 import org.plan.research.minimization.core.algorithm.dd.DDAlgorithmResult
-import org.plan.research.minimization.core.model.DDItem
 import org.plan.research.minimization.core.model.DDContext
+import org.plan.research.minimization.core.model.DDItem
 import org.plan.research.minimization.core.model.PropertyTester
+
 import kotlin.math.min
+import kotlinx.coroutines.yield
 
 /**
  * Default Delta Debugging algorithm.
@@ -15,10 +16,112 @@ import kotlin.math.min
  * so it's not necessary to implement your own.
  */
 class DDMin : DDAlgorithm {
+    @Suppress("NESTED_BLOCK", "TOO_LONG_FUNCTION")
+    override suspend fun <C : DDContext, T : DDItem> minimize(
+        context: C, items: List<T>,
+        propertyTester: PropertyTester<C, T>,
+    ): DDAlgorithmResult<C, T> {
+        var currentContext = context
+        var currentItems = ArrayDeque(items)
+        var smallItems = ArrayDeque<T>()
+        val currentNodes = ArrayDeque<Node>()
+        Node(items.size, parent = null, smallChecked = false, checked = true).let { root ->
+            currentNodes.addAll(root.initNext())
+        }
+        var testSmall = true
 
+        var granularity = 2
+        while (currentItems.size > 1) {
+            var reduced = false
+            while (!reduced) {
+                for (i in 0 until granularity) {
+                    yield()
+                    val node = currentNodes.removeFirst()
+                    repeat(node.size) {
+                        smallItems.add(currentItems.removeFirst())
+                    }
+
+                    if (testSmall) {
+                        if (!node.smallChecked) {
+                            val toBreak = propertyTester.test(currentContext, smallItems)
+                                .isRight { updatedContext ->
+                                    currentContext = updatedContext
+                                    granularity = 2
+                                    smallItems.let {
+                                        smallItems = currentItems
+                                        currentItems = it
+                                    }
+                                    smallItems.clear()
+                                    reduced = true
+                                    node.parent = null
+                                    node.smallChecked = true
+                                    node.checked = true
+                                    currentNodes.clear()
+                                    currentNodes.addAll(node.initNext())
+                                    true
+                                }
+                            if (toBreak) {
+                                break
+                            }
+                        }
+                        node.smallChecked = true
+                        if (granularity == 2) {
+                            currentNodes.single().isCheckedAndMark()
+                        }
+                    } else {
+                        if (granularity == 2 && currentNodes.single().smallChecked) {
+                            node.isCheckedAndMark()
+                        }
+                        if (!node.isCheckedAndMark()) {
+                            val toBreak = propertyTester.test(currentContext, currentItems).isRight { updatedContext ->
+                                currentContext = updatedContext
+                                granularity -= 1
+                                smallItems.clear()
+                                node.delete()
+                                reduced = true
+                                true
+                            }
+                            if (toBreak) {
+                                break
+                            }
+                        }
+                    }
+
+                    currentNodes.add(node)
+                    repeat(node.size) {
+                        currentItems.add(smallItems.removeFirst())
+                    }
+                }
+                if (!reduced) {
+                    if (!testSmall || granularity == 2) {
+                        if (granularity == currentItems.size) {
+                            return DDAlgorithmResult(currentContext, currentItems)
+                        }
+                        granularity = min(granularity * 2, currentItems.size)
+                        val next = currentNodes.flatMap { it.initNext() }
+                        currentNodes.clear()
+                        currentNodes.addAll(next)
+                        testSmall = true
+                    } else {
+                        testSmall = false
+                    }
+                }
+            }
+        }
+        return DDAlgorithmResult(currentContext, currentItems)
+    }
+
+    /**
+     * @property size
+     * @property parent
+     * @property smallChecked
+     * @property checked
+     */
     private class Node(
-        val size: Int, var parent: Node?,
-        var smallChecked: Boolean = false, var checked: Boolean = false
+        val size: Int,
+        var parent: Node?,
+        var smallChecked: Boolean = false,
+        var checked: Boolean = false,
     ) {
         var left: Node? = null
         var right: Node? = null
@@ -38,14 +141,18 @@ class DDMin : DDAlgorithm {
             var node = this
             while (node.parent != null) {
                 val parent = node.parent!!
-                if (node.checked) return true
+                if (node.checked) {
+                    return true
+                }
                 node.checked = true
                 val otherNode = if (parent.left === node) {
                     parent.right
                 } else {
                     parent.left
                 }
-                if (otherNode != null) return false
+                otherNode?.let {
+                    return false
+                }
                 node = parent
             }
             return true
@@ -78,101 +185,12 @@ class DDMin : DDAlgorithm {
             }
 
             while (queue.isNotEmpty()) {
-                val currentNode = queue.removeFirst()
-                currentNode.checked = false
+                val currentNode = queue.removeFirst().apply {
+                    checked = false
+                }
                 currentNode.left?.let { queue.add(it) }
                 currentNode.right?.let { queue.add(it) }
             }
         }
-    }
-
-    override suspend fun <C : DDContext, T : DDItem> minimize(
-        context: C, items: List<T>,
-        propertyTester: PropertyTester<C, T>
-    ): DDAlgorithmResult<C, T> {
-        var currentContext = context
-        var granularity = 2
-        var currentItems = ArrayDeque(items)
-        var smallItems = ArrayDeque<T>()
-        val currentNodes = ArrayDeque<Node>()
-        Node(items.size, parent = null, smallChecked = false, checked = true).let { root ->
-            currentNodes.addAll(root.initNext())
-        }
-        var testSmall = true
-
-        while (currentItems.size > 1) {
-            var reduced = false
-            while (!reduced) {
-                for (i in 0 until granularity) {
-                    yield()
-                    val node = currentNodes.removeFirst()
-                    repeat(node.size) {
-                        smallItems.add(currentItems.removeFirst())
-                    }
-
-                    if (testSmall) {
-                        if (!node.smallChecked) {
-                            val toBreak = propertyTester.test(currentContext, smallItems)
-                                .isRight { updatedContext ->
-                                    currentContext = updatedContext
-                                    granularity = 2
-                                    smallItems.let {
-                                        smallItems = currentItems
-                                        currentItems = it
-                                    }
-                                    smallItems.clear()
-                                    reduced = true
-                                    node.parent = null
-                                    node.smallChecked = true
-                                    node.checked = true
-                                    currentNodes.clear()
-                                    currentNodes.addAll(node.initNext())
-                                    true
-                                }
-                            if (toBreak) break
-                        }
-                        node.smallChecked = true
-                        if (granularity == 2) {
-                            currentNodes.single().isCheckedAndMark()
-                        }
-                    } else {
-                        if (granularity == 2 && currentNodes.single().smallChecked) {
-                            node.isCheckedAndMark()
-                        }
-                        if (!node.isCheckedAndMark()) {
-                            val toBreak = propertyTester.test(currentContext, currentItems).isRight { updatedContext ->
-                                currentContext = updatedContext
-                                granularity -= 1
-                                smallItems.clear()
-                                node.delete()
-                                reduced = true
-                                true
-                            }
-                            if (toBreak) break
-                        }
-                    }
-
-                    currentNodes.add(node)
-                    repeat(node.size) {
-                        currentItems.add(smallItems.removeFirst())
-                    }
-                }
-                if (!reduced) {
-                    if (!testSmall || granularity == 2) {
-                        if (granularity == currentItems.size) {
-                            return DDAlgorithmResult(currentContext, currentItems)
-                        }
-                        granularity = min(granularity * 2, currentItems.size)
-                        val next = currentNodes.flatMap { it.initNext() }
-                        currentNodes.clear()
-                        currentNodes.addAll(next)
-                        testSmall = true
-                    } else {
-                        testSmall = false
-                    }
-                }
-            }
-        }
-        return DDAlgorithmResult(currentContext, currentItems)
     }
 }

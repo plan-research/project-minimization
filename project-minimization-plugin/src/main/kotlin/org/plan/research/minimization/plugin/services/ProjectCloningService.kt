98816da4341c64ea1c33738b4844523eae156d2d
Sergei Kharitontcev-Beglov <Sergei.Kharitontcev-Beglov@jetbrains.com>
2024-10-18 00:53:32 +0200

    Fixes JB-1987

--- project-minimization-plugin/src/main/kotlin/org/plan/research/minimization/plugin/services/ProjectCloningService.kt
+++ project-minimization-plugin/src/main/kotlin/org/plan/research/minimization/plugin/services/ProjectCloningService.kt
@@ -3,17 +3,23 @@ package org.plan.research.minimization.plugin.services
 import org.plan.research.minimization.plugin.settings.MinimizationPluginSettings
 
 import com.intellij.ide.impl.ProjectUtil
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.application.smartReadAction
 import com.intellij.openapi.application.writeAction
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.project.guessProjectDir
 import com.intellij.openapi.project.isProjectOrWorkspaceFile
-import com.intellij.openapi.vfs.VfsUtil
-import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.openapi.vfs.findOrCreateDirectory
+import com.intellij.openapi.vfs.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.nio.file.Path
 
 import java.util.*
+import kotlin.io.path.copyTo
+import kotlin.io.path.createDirectories
 
 import kotlin.io.path.pathString
 import kotlin.io.path.relativeTo
@@ -40,14 +46,14 @@ class ProjectCloningService(private val rootProject: Project) {
      */
     suspend fun clone(project: Project): Project? {
         val projectRoot = project.guessProjectDir() ?: return null
+
         val clonedProjectPath = writeAction {
             val clonedProjectPath = createNewProjectDirectory()
             val snapshotLocation = getSnapshotLocation()
-            VfsUtil.copyDirectory(
-                this,
-                projectRoot,
-                clonedProjectPath,
-            ) { !it.path.startsWith(snapshotLocation.path) && isImportant(it, projectRoot) }
+            projectRoot.copyTo(clonedProjectPath.toNioPath()) {
+                isImportant(it, projectRoot) &&
+                        it.path != snapshotLocation.path
+            }
             clonedProjectPath
         }
         return ProjectUtil.openOrImportAsync(clonedProjectPath.toNioPath())
@@ -70,4 +76,16 @@ class ProjectCloningService(private val rootProject: Project) {
             .guessProjectDir()
             ?.findOrCreateDirectory(tempProjectsDirectoryName)
             ?: rootProject.guessProjectDir()!!
+
+    private fun VirtualFile.copyTo(destination: Path, root: Boolean = true, filter: (VirtualFile) -> Boolean) {
+        if (!filter(this)) return
+        val originalPath = this.toNioPathOrNull() ?: return
+        val fileDestination = if (root) destination else destination.resolve(name)
+        originalPath.copyTo(fileDestination, overwrite = true)
+        if (isDirectory) {
+            for (child in children) {
+                child.copyTo(fileDestination, false, filter)
+            }
+        }
+    }
 }

c136540b1b2ca66deab480ae808db2dbeb511a2e
Kristina <kristina.trofimova@jetbrains.com>
2024-11-19 21:10:22 +0200

    ProjectCloningGitSnapshotManager

--- project-minimization-plugin/src/main/kotlin/org/plan/research/minimization/plugin/services/ProjectCloningGitService.kt
+++ project-minimization-plugin/src/main/kotlin/org/plan/research/minimization/plugin/services/ProjectCloningGitService.kt
@@ -2,60 +2,32 @@ package org.plan.research.minimization.plugin.services
 
 import com.intellij.openapi.application.readAction
 import com.intellij.openapi.components.Service
-import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.project.guessProjectDir
 import com.intellij.openapi.project.isProjectOrWorkspaceFile
-import com.intellij.openapi.util.io.findOrCreateDirectory
-import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.toNioPathOrNull
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import org.plan.research.minimization.plugin.model.HeavyIJDDContext
 import org.plan.research.minimization.plugin.model.IJDDContext
-import org.plan.research.minimization.plugin.model.LightIJDDContext
-import java.nio.file.Path
 import java.util.*
 import kotlin.io.path.pathString
 import kotlin.io.path.relativeTo
 import org.eclipse.jgit.api.Git
-import java.io.File
 
 @Service(Service.Level.PROJECT)
 class ProjectCloningGitService(private val rootProject: Project) {
-    private val openingService = service<ProjectOpeningService>()
-    private val tempProjectsDirectoryName by rootProject
-        .service<MinimizationPluginSettings>()
-        .stateObservable
-        .temporaryProjectLocation
-        .observe { it }
     private val importantFiles = setOf("modules.xml", "misc.xml", "libraries")
 
-    suspend fun clone(context: IJDDContext): IJDDContext? =
-        when (context) {
-            is HeavyIJDDContext -> clone(context)
-            is LightIJDDContext -> clone(context)
+    suspend fun commitChanges(context: IJDDContext): IJDDContext {
+        return context.apply {
+            commit(this.projectDir)
+            projectDir.refresh(false, true)
         }
-
-    suspend fun clone(context: LightIJDDContext): LightIJDDContext? {
-        val clonedPath = cloneProjectImpl(context.projectDir)
-        val clonedProjectDir = LocalFileSystem.getInstance().refreshAndFindFileByNioFile(clonedPath) ?: return null
-        clonedProjectDir.refresh(false, true)
-        return context.copy(projectDir = clonedProjectDir)
     }
 
-    suspend fun clone(context: HeavyIJDDContext): HeavyIJDDContext? {
-        val clonedPath = cloneProjectImpl(context.projectDir)
-        val clonedProject = openingService.openProject(clonedPath) ?: return null
-        LocalFileSystem.getInstance().refreshAndFindFileByNioFile(clonedPath)?.refresh(false, true)
-        return context.copy(project = clonedProject)
-    }
-
-    private suspend fun cloneProjectImpl(projectDir: VirtualFile): Path {
-        projectDir.refresh(false, true)
+    private suspend fun commit(projectDir: VirtualFile) {
+        projectDir.refresh(false, true) // ???
         return withContext(Dispatchers.IO) {
-            //TODO Git.init().setDirectory(projectDir.toNioPath().toFile()).call()
             Git.open(projectDir.toNioPath().toFile()).let {
                 projectDir.gitAdd(it) { file ->
                     isImportant(file, projectDir)
@@ -63,7 +35,6 @@ class ProjectCloningGitService(private val rootProject: Project) {
                 it.commit().setMessage(UUID.randomUUID().toString()).call()
                 it.close()
             }
-            projectDir.toNioPath()
         }
     }
 
@@ -81,7 +52,7 @@ class ProjectCloningGitService(private val rootProject: Project) {
             return
         }
         val originalPath = this.toNioPathOrNull() ?: return
-        if (originalPath.toString().contains(".git")) {
+        if (originalPath.toString().contains(".git")) { // merge with the filter
             return
         }
         try {

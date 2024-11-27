fca5897b1b9c280cf348c6896b8ef7c83385f7e1
Sergei Kharitontcev-Beglov <Sergei.Kharitontcev-Beglov@jetbrains.com>
2024-11-27 11:00:45 +0100

    Added test for overridden composing

--- project-minimization-plugin/src/main/kotlin/org/plan/research/minimization/plugin/psi/KotlinOverriddenElementsGetter.kt
+++ project-minimization-plugin/src/main/kotlin/org/plan/research/minimization/plugin/psi/KotlinOverriddenElementsGetter.kt
@@ -3,6 +3,8 @@ package org.plan.research.minimization.plugin.psi
 import org.jetbrains.kotlin.analysis.api.analyze
 import org.jetbrains.kotlin.analysis.api.symbols.psi
 import org.jetbrains.kotlin.analysis.api.symbols.sourcePsi
+import org.jetbrains.kotlin.analysis.api.types.symbol
+import org.jetbrains.kotlin.psi.KtClass
 import org.jetbrains.kotlin.psi.KtElement
 import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.KtProperty
@@ -11,6 +13,7 @@ object KotlinOverriddenElementsGetter {
     fun getOverriddenElements(element: KtElement): List<KtElement> = when (element) {
         is KtNamedFunction -> getOverriddenFunction(element)
         is KtProperty -> getOverriddenProperties(element)
+        // is KtClass -> getOverriddenClass(element) JBRes-2212
         else -> emptyList()
     }
 
@@ -30,5 +33,14 @@ object KotlinOverriddenElementsGetter {
             .mapNotNull { it.sourcePsi() }
             .filterIsInstance<KtElement>()
             .toList()
+    }
+
+    private fun getOverriddenClass(element: KtClass) = analyze(element) {
+        val symbol = element.classSymbol
+        symbol
+            ?.superTypes
+            ?.mapNotNull { type -> type.symbol?.sourcePsi<KtElement>() }
+            ?.toList()
+            ?: emptyList()
     }
 }

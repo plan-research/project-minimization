949f1a9fa2a671d0e6f0abdae562d2d39400127b
InversionSpaces <inversionspaces@vivaldi.net>
2025-02-19 15:03:20 +0100

    Constrain test

--- project-minimization-core/src/test/kotlin/org/plan/research/minimization/core/algorithm/dd/impl/DDDomain.kt
+++ project-minimization-core/src/test/kotlin/org/plan/research/minimization/core/algorithm/dd/impl/DDDomain.kt
@@ -5,21 +5,37 @@ import net.jqwik.api.Arbitrary
 import net.jqwik.api.Provide
 import net.jqwik.api.domains.DomainContextBase
 import org.plan.research.minimization.core.algorithm.dd.impl.DDAlgorithmTestBase.SomeDDItem
+import kotlin.test.assertContains
+import kotlin.math.min
 
 class DDDomain : DomainContextBase() {
     @Provide
     fun testCase(): Arbitrary<TestCase> {
-        val itemsA = Arbitraries.integers().between(1, 1000).map { n -> List(n) { SomeDDItem(it) } }
+        val itemsA = Arbitraries.integers().between(1, MAX_ITEMS).map { n -> List(n) { SomeDDItem(it) } }
         return itemsA.flatMap { items ->
-            Arbitraries.integers().between(1, items.size).map {
-                val target = items.take(it)
-                TestCase(items, target)
+            Arbitraries.integers().between(1, min(MAX_TARGET, items.size)).flatMap { nTarget ->
+                val target = items.take(nTarget)
+                Arbitraries.integers().between(0, target.size).map { nImportant ->
+                    val important = target.take(nImportant)
+                    TestCase(items, target, important)
+                }
             }
         }
+    }
+
+    companion object {
+        const val MAX_ITEMS = 100
+        const val MAX_TARGET = 4
     }
 }
 
 data class TestCase(
     val items: List<SomeDDItem>,
     val target: List<SomeDDItem>,
-)
+    val important: List<SomeDDItem>,
+) {
+    init {
+        target.forEach { assertContains(items, it) }
+        important.forEach { assertContains(target, it) }
+    }
+}
